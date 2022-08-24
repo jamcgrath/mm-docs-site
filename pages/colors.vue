28b4deaff65f7f109a0302f2e1066359bc74bc3e
James McGrath <james@jamesmcgrath.net>
2022-08-24 17:40:26 +0900

    add colors page

--- pages/colors.vue
+++ pages/colors.vue
@@ -1,10 +1,135 @@
 <template>
-	<div class="page-colors p-4">COLORS PAGE</div>
+	<div class="page-colors p-4">
+		<div class="page-content grid-fluid gap-5">
+			<div
+				class="color-card flex flex-col"
+				v-for="(color, index) in colorVarNames"
+				:key="index"
+			>
+				<div
+					class="color-value w-100 p-4 cursor-pointer flex-1"
+					:class="getColor(colorVars[color])"
+					:style="{ background: `var(${color})` }"
+					@click="copyToClipboard($event)"
+					:data-item="colorVars[color]"
+				>
+					<span class="pointer-events-none">{{ colorVars[color] }}</span>
+					<span class="copy-msg">copied</span>
+				</div>
+				<ul class="p-3 m-0" @click="copyToClipboard($event)">
+					<li class="cursor-pointer" :data-item="color">
+						{{ color }}
+						<span class="copy-msg">copied</span>
+					</li>
+					<li
+						v-if="!colorVars[color].includes('gradient')"
+						class="mt-1_5 cursor-pointer"
+						:data-item="colorNames[index]"
+					>
+						{{ colorNames[index] }}
+						<span class="copy-msg">copied</span>
+					</li>
+					<li
+						class="cursor-pointer mt-1_5"
+						:data-item="`bg-${colorNames[index]}`"
+					>
+						{{ `bg-${colorNames[index]}` }}
+						<span class="copy-msg">copied</span>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-	export default {}
+	import colorsProps from '~/mm-css/dist/json/colors-props.json'
+	import colorsAlerts from '~/mm-css/dist/json/colors-alerts.json'
+	import colorsAlertsBg from '~/mm-css/dist/json/colors-alerts-bg.json'
+	import colorsGray from '~/mm-css/dist/json/colors-gray.json'
+	import colorsNeutral from '~/mm-css/dist/json/colors-neutral.json'
+	import colorsState from '~/mm-css/dist/json/colors-state.json'
+	import colorGradients from '~/mm-css/dist/json/colors-gradients.json'
+	import colorBrand from '~/mm-css/dist/json/colors-brand.json'
+	import colorBrandBg from '~/mm-css/dist/json/colors-brand-bg.json'
+	import colorGrayBg from '~/mm-css/dist/json/colors-gray-bg.json'
+	import colorNeutralBg from '~/mm-css/dist/json/colors-neutral-bg.json'
+	import colorStateBg from '~/mm-css/dist/json/colors-state-bg.json'
+	export default {
+		computed: {
+			colorVarNames() {
+				return Object.keys(colorsProps[':root'])
+			},
+			colorNames() {
+				return this.colorVarNames.map((color) => color.slice(2))
+			},
+			colorVars() {
+				return colorsProps[':root']
+			},
+			allColors() {
+				return {
+					colorsProps,
+					colorsAlerts,
+					colorsAlertsBg,
+					colorsGray,
+					colorsNeutral,
+					colorsState,
+					colorGradients,
+					colorBrand,
+					colorBrandBg,
+					colorGrayBg,
+					colorNeutralBg,
+					colorStateBg,
+				}
+			},
+		},
+		methods: {
+			getColor(color) {
+				const l = parseInt(color.split(',')[2])
+				return l <= 50 ? 'white' : 'black'
+			},
+			async copyToClipboard(e) {
+				const el = e.target
+				const text = el.dataset.item
+				try {
+					await navigator.clipboard.writeText(text)
+					el.classList.add('copied')
+				} catch (err) {
+					console.error('Failed to copy text: ', err)
+				} finally {
+					setTimeout(() => {
+						el.classList.remove('copied')
+					}, 1000)
+				}
+			},
+		},
+		mounted() {},
+	}
 </script>
 
 <style>
+	.page-content {
+		--col-width: 250px;
+		display: grid;
+	}
+	.color-card {
+		border: 1px solid var(--gray-4);
+		border-radius: 15px;
+		overflow: hidden;
+	}
+	.color-card ul {
+		min-height: 100px;
+	}
+	.copy-msg {
+		display: none;
+		font-size: 0.9em;
+		font-family: monospace;
+		text-decoration-line: underline;
+		text-underline-offset: 4px;
+		text-transform: capitalize;
+		margin-left: 10px;
+	}
+	.copied > .copy-msg {
+		display: inline-block;
+	}
 </style>
