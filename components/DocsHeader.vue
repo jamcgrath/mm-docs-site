837f004cd94b1859a56c567c09aaff7cd7276817
James McGrath <james@jamesmcgrath.net>
2022-08-27 16:10:03 +0900

    add off canvas sidebar

--- components/DocsHeader.vue
+++ components/DocsHeader.vue
@@ -1,14 +1,36 @@
 <template>
-	<header class="mmd-header bg-bw-primary white p-4 fw-6">
+	<header class="mmd-header bg-bw-primary white px-4 py-1_5 fw-6 flex j-content-between a-items-center" ref="header">
 		<h1 class="title-6">MM-Docs</h1>
+		<div class="mmd-header-buttons">
+			<button class="mmd-header-button_code cursor-pointer" @click="openOffCanvas">
+				<span class="icon-code" aria-hidden="true"></></span>
+				<span class="visually-hidden">Open Code View</span>
+			</button>
+		</div>
 	</header>
 </template>
 
 <script>
-	export default {}
+	export default {
+		methods: {
+			openOffCanvas() {
+				this.$refs.header.classList.add('open-ocs')
+			},
+		},
+	}
 </script>
 
-<style>
-	.mmd-header {
+<style scoped>
+	.mmd-header-buttons button {
+		background: transparent;
+		border: 0;
+		color: inherit;
+		padding: 0;
+		min-width: 40px;
+	}
+	.icon-code {
+		font-family: monospace;
+		font-size: 14px;
+		font-weight: 800;
 	}
 </style>
---
+++ components/DocsOffCanvasSidebar.vue
@@ -0,0 +1,27 @@
+<template>
+	<aside class="off-canvas-sidebar p4 bg-white" @keydown.esc="close">
+		<button @click="close">Close</button>
+		<div class="code-view">OFF CANVAS SIDEBAR</div>
+	</aside>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {}
+		},
+		methods: {
+			close() {
+				document.querySelector('.mmd-header ').classList.remove('open-ocs')
+			},
+		},
+	}
+</script>
+
+<style>
+	.off-canvas-sidebar {
+		border-left: 1px solid var(--gray-5);
+		min-height: 100vh;
+		overflow-y: auto;
+	}
+</style>
