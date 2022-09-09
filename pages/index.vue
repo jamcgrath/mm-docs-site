43ad1e07dfa2cedee77c89cb829de417091c9a73
James McGrath <james@jamesmcgrath.net>
2022-09-09 16:57:07 +0900

    grid all classes

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,41 @@
 <template>
-	<h1>HOME</h1>
+	<ul class="list-reset grid grid-fluid">
+		<li v-for="(item, index) in filesNamesArray" :key="index" class="mt-3 pl-2">
+			<nuxt-link :to="`/${item}`" class="td-underline_hover">{{
+				item
+			}}</nuxt-link>
+		</li>
+	</ul>
 </template>
 
 <script>
-	export default {}
+	import filenames from '~/mm-css/dist/json/filenames.json'
+	export default {
+		data() {
+			return {
+				excludeList: ['media-queries'],
+			}
+		},
+		computed: {
+			filesNamesArray() {
+				const fileList = []
+				Object.values(filenames).forEach((file) => {
+					let isExcluded = false
+					this.excludeList.forEach((excluded) => {
+						if (file.includes(excluded)) {
+							isExcluded = true
+						}
+					})
+					if (isExcluded) {
+						return
+					} else {
+						fileList.push(file.split('.')[0])
+					}
+				})
+				return fileList
+			},
+		},
+	}
 </script>
 
 <style>
