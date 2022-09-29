eff34f501226c65b5d80c419c4ce4037a03e1b8d
James McGrath <james@jamesmcgrath.net>
2022-09-29 09:31:02 +0900

    add mm-icons.css

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['~/mm-css/dist/styles.css'],
+  css: ['~/mm-css/dist/styles.css', '~/mm-icons/dist/mm-icons.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
