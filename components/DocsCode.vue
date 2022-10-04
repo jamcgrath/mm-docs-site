dd9bba7259db73d8db7d8352459a9f0adbc5220f
James McGrath <james@jamesmcgrath.net>
2022-10-05 05:29:38 +0900

    new component

---
+++ components/DocsCode.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="code-preview overflow-auto relative">
+		<pre
+			class="m-0"
+			v-for="(example, index) in codeExample"
+			:key="index"
+			@click="$copyToClipboard(example.code)"
+		>
+						<code v-highlight="example.code" :class="`language-${example.language}`"></code>
+					</pre>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: {
+			codeExample: Object,
+		},
+	}
+</script>
+
+<style>
+</style>
