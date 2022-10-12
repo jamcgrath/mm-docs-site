e4811ab680b4d7cd876a674e9ed819de2763c86d
Trinmar Boado <timi.boado@gmail.com>
2022-10-12 08:45:58 +0800

    aria compliance

--- components/mm-components/pagination/MmPagination.vue
+++ components/mm-components/pagination/MmPagination.vue
@@ -1,38 +1,46 @@
 <template>
-	<div>
-    <button v-show="pages > 5 && model > 3" type="button" class="btn btn-white btn-med box-shadow-med arrow">
-      <i aria-hidden="true" class="mmi mmi-chevron-left icon"></i>
-    </button>
-    <button
-      v-show="pages > 5 && model > 5"
-      type="button" class="btn btn-med box-shadow-med fw-500 gray-7 ml-1 btn-white"
-      @click="model -= 5"
-    >
-      ...
-    </button>
-    <button
-      v-for="(i) in paginator"
-      :key="'btn' + i"
-      type="button" class="btn btn-med box-shadow-med fw-500 m-1 width-40 btn-white"
-      :class="{
-        'btn-secondary': i + 1 === model,
-        'gray-7': i + 1 !== model
-      }"
-      @click="model = i + 1"
-    >
-			{{ i + 1 }}
-		</button>
-    <button
-      v-show="pages > 5 && model < pages - 5"
-      type="button" class="btn btn-med box-shadow-med fw-500 gray-7 mr-1 btn-white"
-      @click="model += 5"
-    >
-      ...
-    </button>
-    <button v-show="pages > 5 && model < pages - 3" type="button" class="btn btn-white btn-med box-shadow-med arrow btn-white">
-      <i aria-hidden="true" class="mmi mmi-chevron-right icon"></i>
-    </button>
-  </div>
+  <nav aria-label="pagination">
+    <ul class="p-0 m-0">
+      <li v-show="pages > 5 && model > 3" type="button" class="btn btn-white btn-med box-shadow-med arrow"
+        @click="model--"
+      >
+        <i aria-hidden="true" class="mmi mmi-chevron-left icon"></i>
+      </li>
+      <li
+        v-show="pages > 5 && model > 5"
+        type="button" class="btn btn-med box-shadow-med fw-500 gray-7 ml-1 btn-white"
+        @click="model -= 5"
+      >
+        ...
+      </li>
+      <li
+        v-for="(i) in paginator"
+        :key="'btn' + i"
+        type="button" class="btn btn-med box-shadow-med fw-500 m-1 width-40 btn-white"
+        :class="{
+          'btn-secondary': i + 1 === model,
+          'gray-7': i + 1 !== model
+        }"
+        @click="model = i + 1"
+        :tabindex="i+1 === model ? 0 : -1"
+        :aria-label="`page ${i+1}`" :aria-checked="i+1 === model"
+      >
+        {{ i + 1 }}
+      </li>
+      <li
+        v-show="pages > 5 && model < pages - 5"
+        type="button" class="btn btn-med box-shadow-med fw-500 gray-7 mr-1 btn-white"
+        @click="model += 5"
+      >
+        ...
+      </li>
+      <li v-show="pages > 5 && model < pages - 3" type="button" class="btn btn-white btn-med box-shadow-med arrow btn-white"
+        @click="model++"
+      >
+        <i aria-hidden="true" class="mmi mmi-chevron-right icon"></i>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <script>
@@ -43,10 +51,14 @@
       },
       pages: {
         type: Number,
-        deafult: 100
+        deafult: 0
       }
     },
-    watch: {},
+    watch: {
+      model (newVal) {
+        this.$emit('input', newVal)
+      }
+    },
     data () {
       return {
         model: this.value || 2
@@ -59,7 +71,7 @@
       paginator () {
         const currentIndex = this.model
         const arr = this.pagesArray
-        console.log('pagesArray', arr)
+        // console.log('pagesArray', arr)
         if (this.model <= 3) return arr.slice(0, 5)
         if (this.model >= this.pages - 3) return arr.slice(this.pages - 5)
         return arr.slice(currentIndex - 3, currentIndex - 3 + 5)
