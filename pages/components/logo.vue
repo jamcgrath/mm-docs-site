9ac94d049ec598a52a265ef02d3e295fb46436ad
James <jamcgrath@users.noreply.github.com>
2022-10-13 08:57:33 +0900

    mm-logo

---
+++ pages/components/logo.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="">
+		<mm-logo></mm-logo>
+	</div>
+</template>
+
+<script>
+	import MmLogo from '~/components/mm-components/logo/MmLogo.vue'
+	export default {
+		layout: 'layout-components',
+		components: {
+			MmLogo,
+		},
+	}
+</script>
+
+<style>
+</style>
