51ef75c9f5c40ee3ff0be2ca89a700f76dd50d47
James <jamcgrath@users.noreply.github.com>
2022-10-13 11:33:37 +0900

    fix pagination keyboard navigation

--- components/mm-components/pagination/MmPagination.vue
+++ components/mm-components/pagination/MmPagination.vue
@@ -1,116 +1,137 @@
 <template>
-  <nav aria-label="pagination">
-    <ul class="p-0 m-0">
-      <li v-show="pages > 5 && model > 3" type="button" class="btn btn-white btn-med box-shadow-med arrow"
-        @click="model--"
-      >
-        <i aria-hidden="true" class="mmi mmi-chevron-left icon"></i>
-      </li>
-      <li
-        v-show="pages > 5 && model > 5"
-        type="button" class="btn btn-med box-shadow-med fw-500 gray-7 ml-1 btn-white"
-        @click="model -= 5"
-      >
-        ...
-      </li>
-      <li
-        v-for="(i) in paginator"
-        :key="'btn' + i"
-        type="button" class="btn btn-med box-shadow-med fw-500 m-1 width-40 btn-white"
-        :class="{
-          'btn-secondary': i + 1 === model,
-          'gray-7': i + 1 !== model
-        }"
-        @click="model = i + 1"
-        :tabindex="i+1 === model ? 0 : -1"
-        :aria-label="`page ${i+1}`" :aria-checked="i+1 === model"
-      >
-        {{ i + 1 }}
-      </li>
-      <li
-        v-show="pages > 5 && model < pages - 5"
-        type="button" class="btn btn-med box-shadow-med fw-500 gray-7 mr-1 btn-white"
-        @click="model += 5"
-      >
-        ...
-      </li>
-      <li v-show="pages > 5 && model < pages - 3" type="button" class="btn btn-white btn-med box-shadow-med arrow btn-white"
-        @click="model++"
-      >
-        <i aria-hidden="true" class="mmi mmi-chevron-right icon"></i>
-      </li>
-    </ul>
-  </nav>
+	<nav aria-label="pagination">
+		<ul class="p-0 m-0 flex a-items-center pagination-list">
+			<li v-show="pages > 5 && model > 3">
+				<button
+					type="button"
+					class="btn btn-pagination btn-med box-shadow-med arrow"
+					@click="model--"
+				>
+					<i aria-hidden="true" class="mmi mmi-chevron-left icon"></i>
+					<span class="visually-hidden">Previous page</span>
+				</button>
+			</li>
+			<li v-show="pages > 5 && model > 5">
+				<button
+					type="button"
+					class="btn btn-med box-shadow-med fw-500 gray-7 ml-1 btn-pagination"
+					@click="model -= 5"
+				>
+					...<span class="visually-hidden">Previous 5 pages</span>
+				</button>
+			</li>
+			<li v-for="i in paginator" :key="'btn' + i">
+				<button
+					type="button"
+					class="btn btn-med box-shadow-med fw-500 m-1 width-40 btn-pagination"
+					:class="{
+						'btn-secondary': i + 1 === model,
+						'gray-7': i + 1 !== model,
+					}"
+					@click="model = i + 1"
+					:aria-label="`page ${i + 1}`"
+					:aria-current="i + 1 === model ? 'page' : false"
+				>
+					{{ i + 1 }}
+				</button>
+			</li>
+			<li v-show="pages > 5 && model < pages - 5">
+				<button
+					type="button"
+					class="btn btn-med box-shadow-med fw-500 gray-7 mr-1 btn-pagination"
+					@click="model += 5"
+				>
+					...<span class="visually-hidden">Next 5 pages</span>
+				</button>
+			</li>
+			<li v-show="pages > 5 && model < pages - 3">
+				<button
+					type="button"
+					class="btn btn-pagination btn-med box-shadow-med arrow"
+					@click="model++"
+				>
+					<i aria-hidden="true" class="mmi mmi-chevron-right icon"></i
+					><span class="visually-hidden">Next page</span>
+				</button>
+			</li>
+		</ul>
+	</nav>
 </template>
 
 <script>
 	export default {
-    props: {
-      value: {
-        type: Number,
-      },
-      pages: {
-        type: Number,
-        deafult: 0
-      }
-    },
-    watch: {
-      model (newVal) {
-        this.$emit('input', newVal)
-      }
-    },
-    data () {
-      return {
-        model: this.value || 2
-      }
-    },
-    computed: {
-      pagesArray () {
-        return [...Array(this.pages).keys()]
-      },
-      paginator () {
-        const currentIndex = this.model
-        const arr = this.pagesArray
-        // console.log('pagesArray', arr)
-        if (this.model <= 3) return arr.slice(0, 5)
-        if (this.model >= this.pages - 3) return arr.slice(this.pages - 5)
-        return arr.slice(currentIndex - 3, currentIndex - 3 + 5)
-      }
-    },
-    methods: {
-    }
+		props: {
+			value: {
+				type: Number,
+			},
+			pages: {
+				type: Number,
+				deafult: 0,
+			},
+		},
+		watch: {
+			model(newVal) {
+				this.$emit('input', newVal)
+			},
+		},
+		data() {
+			return {
+				model: this.value || 1,
+			}
+		},
+		computed: {
+			pagesArray() {
+				return [...Array(this.pages).keys()]
+			},
+			paginator() {
+				const currentIndex = this.model
+				const arr = this.pagesArray
+				// console.log('pagesArray', arr)
+				if (this.model <= 3) return arr.slice(0, 5)
+				if (this.model >= this.pages - 3) return arr.slice(this.pages - 5)
+				return arr.slice(currentIndex - 3, currentIndex - 3 + 5)
+			},
+		},
+		methods: {},
 	}
 </script>
 
 <style>
-.gray-7 {
-  color: var(--gray-7)
-}
+	.gray-7 {
+		color: var(--gray-7);
+	}
 
-.fw-500 {
-  font-weight: 500;
-}
+	.fw-500 {
+		font-weight: 500;
+	}
+	.btn-pagination {
+		--btn-bg: var(--white);
+		min-height: 34px;
+		width: 40px;
+	}
 
-.width-40  {
-  width: 40px !important; 
-}
+	.btn-pagination[aria-current] {
+		--btn-bg: var(--grape);
+	}
+	.btn-pagination:hover {
+		--btn-bg: var(--grape-10);
+	}
 
-.btn-white:hover {
-  background-color: var(--grape-10) !important;
-}
+	.btn-pagination:active {
+		--btn-bg: var(--grape-light);
+	}
+	.btn-pagination:focus {
+		--btn-focus-color: currentcolor;
+	}
 
-.btn-white:active {
-  background-color: var(--grape-light) !important;
-}
+	.icon {
+		position: relative;
+		top: 2px;
+	}
 
-.icon {
-  position: relative;
-  top: 2px;
-}
-
-.arrow {
-  width: 40px;
-  height: 40px;
-  color: var(--navy-dark);
-}
+	.arrow {
+		width: 40px;
+		height: 40px;
+		color: var(--navy-dark);
+	}
 </style>
