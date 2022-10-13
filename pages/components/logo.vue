691d8edf9be2874cf99127c86b02bf8c159865e5
James <jamcgrath@users.noreply.github.com>
2022-10-13 09:09:22 +0900

    add logo mark

--- pages/components/logo.vue
+++ pages/components/logo.vue
@@ -1,15 +1,21 @@
 <template>
 	<div class="">
 		<mm-logo></mm-logo>
+		<mm-logo width="200px" fill="red"></mm-logo>
+		<hr />
+		<mm-logo-mark></mm-logo-mark>
+		<mm-logo-mark width="25px" fill="orange"></mm-logo-mark>
 	</div>
 </template>
 
 <script>
 	import MmLogo from '~/components/mm-components/logo/MmLogo.vue'
+	import MmLogoMark from '~/components/mm-components/logo/MmLogoMark.vue'
 	export default {
 		layout: 'layout-components',
 		components: {
 			MmLogo,
+			MmLogoMark,
 		},
 	}
 </script>
