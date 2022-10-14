486afb291b7288b313c2faf526308929de2849c8
James <jamcgrath@users.noreply.github.com>
2022-10-14 12:37:16 +0900

    fix emit

--- components/mm-components/newsletter/MmNewsletter.vue
+++ components/mm-components/newsletter/MmNewsletter.vue
@@ -1,32 +1,38 @@
 <template>
-	<div class="newsletter flex flex-col br-12"
-    :class="{
-      'grape-light-bg newsletter-selected': selected
-    }"
-  >
-    <img src="https://picsum.photos/350/150" class="img" />
-    <div class="mm-body m-4 navy-dark">
-      <div class="flex title-bar">
-        <div class="flex-1 title-5_semibold m-0 p-0 ">
-          {{ value.title }}
-        </div>
-        <button type="button" class="btn btn-sm btn-circle"
-          :class="{
-            'btn-selected': selected
-          }"
-          @click="$emit('select', value), selected = !selected"
-        >
-          <i aria-hidden="true" :class="`mmi mmi-${selected ? 'check' : 'plus'}`"></i>
-        </button>
-      </div>
-      <div class="caption" :class="{ 'gray-5': !selected }">
-        {{ value.frequency }}
-      </div>
-      <div class="caption mt-2" :class="{ 'gray-7': !selected }">
-        {{ value.description }}
-      </div>
-    </div>
-  </div>
+	<div
+		class="newsletter flex flex-col br-12"
+		:class="{
+			'grape-light-bg newsletter-selected': selected,
+		}"
+	>
+		<img src="https://picsum.photos/350/150" class="img" alt="" />
+		<div class="mm-body m-4 navy-dark">
+			<div class="flex title-bar">
+				<div class="flex-1 title-5_semibold m-0 p-0">
+					{{ value.title }}
+				</div>
+				<button
+					type="button"
+					class="btn btn-sm btn-circle"
+					:class="{
+						'btn-selected': selected,
+					}"
+					@click="select()"
+				>
+					<i
+						aria-hidden="true"
+						:class="`mmi mmi-${selected ? 'check' : 'plus'}`"
+					></i>
+				</button>
+			</div>
+			<div class="caption" :class="{ 'gray-5': !selected }">
+				{{ value.frequency }}
+			</div>
+			<div class="caption mt-2" :class="{ 'gray-7': !selected }">
+				{{ value.description }}
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -34,65 +40,56 @@
 		props: {
 			value: {
 				type: Object,
-        default () {
-          return {
-            id: this.$nanoid(),
-            title: 'Mamamia Daily',
-            frequency: 'Daily',
-            description: 'Get across the stories women are talking about today.'
-          }
-        }
-			}
-		},
-		watch: {
-      selected (newVal) {
-        // console.log('selected: ' + newVal)
-        console.log('this.$emit', this.$emit)
-        this.$emit('select', newVal)
-        if (newVal) {
-          this.$emit('select', newVal)
-        } else {
-          this.$emit('deselect', newVal)
-        }
-      }
+				default() {
+					return {
+						id: this.$nanoid(),
+						title: 'Mamamia Daily',
+						frequency: 'Daily',
+						description: 'Get across the stories women are talking about today.',
+					}
+				},
+			},
 		},
 		data() {
 			return {
-        selected: false
+				selected: false,
 			}
 		},
-		computed: {
+		computed: {},
+		methods: {
+			select() {
+				this.selected = !this.selected
+				this.$emit('select', this.selected)
+			},
 		},
-		methods: {},
 	}
 </script>
 
 <style>
+	.btn {
+		background: rgba(0, 0, 0, 0.1);
+	}
+	.btn-selected {
+		background: var(--grape);
+		color: var(--white);
+	}
 
-.btn {
-  background: rgba(0, 0, 0, 0.1);
-}
-.btn-selected {
-  background: var(--grape);
-  color: var(--white)
-}
+	.newsletter {
+		width: 327px;
+		max-height: 183.7px;
+		border: 1px solid var(--gray-4);
+	}
 
-.newsletter {
-  width: 327px;
-  max-height: 183.7px;
-  border: 1px solid var(--gray-4);
-}
+	.newsletter-selected {
+		border: 2px solid var(--grape);
+	}
 
-.newsletter-selected {
-  border: 2px solid var(--grape);
-}
-
-.img {
-  position: relative;
-  bottom: 0px;
-  border-radius: 11.5px 11.5px 0 0;
-  height: 65.7px;
-  width: 100%;
-  object-fit: cover;
-}
+	.img {
+		position: relative;
+		bottom: 0px;
+		border-radius: 11.5px 11.5px 0 0;
+		height: 65.7px;
+		width: 100%;
+		object-fit: cover;
+	}
 </style>
