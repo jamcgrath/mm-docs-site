cb9815a943dc23a729d2a2daed6789f535bc0aa7
Trinmar Boado <timi.boado@gmail.com>
2022-10-17 16:55:24 +0800

    version 1

---
+++ components/mm-components/flag/MmFlag.vue
@@ -0,0 +1,50 @@
+<template>
+	<div class="flex p-4 navy-dark-bg gray-4 br-8">
+		<div v-if="icon" class="mr-3" >
+			<i aria-hidden="true" :class="icon"></i>
+		</div>
+		<div class="flex-1">
+			<div class="caption_med">{{ title || 'No title'}}</div>
+			<div v-if="body" class="small">{{ body }}</div>
+		</div>
+		<div class="flex a-items-center">
+			<button v-if="button || false" type="button" class="btn btn-flat-secondary btn-sm"
+				@click="$emit('buttonClick')"
+			>
+				{{ button }}
+			</button>
+			<!-- <button type="button" class="btn btn-flat-secondary btn-sm btn-circle">
+				<i aria-hidden="true" class="mmi mmi-close"></i>
+			</button> -->
+			<button v-if="hasClose || false" type="button" class="btn btn-flat-primary btn-sm btn-circle ml-2"
+				@click="$emit('closeClick')"
+			>
+				<i aria-hidden="true" class="mmi mmi-close"></i>
+			</button>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: {
+			icon: String,
+			title: String,
+			body: String,
+			button: String,
+			hasClose: Boolean
+		},
+		mounted() {
+		},
+		computed: {
+		},
+		methods: {
+    }
+	}
+</script>
+
+<style scoped>
+.close {
+
+}
+</style>
---
+++ pages/components/flag.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="gutter">
+    <mmFlag class="mb-2" title="Trinmar Pogi" />
+    <mmFlag class="mb-2" icon="mmi mmi-multiply-incircle-filled error" title="Error" body="error hahaaha" />
+		<mmFlag class="mb-2" icon="mmi mmi-check-circle-filled success" title="Success" body="Success huhuhu"
+      button="click me" @buttonClick="log('ahhh!')" has-close @closeClick="log('oh oh!')" />
+	</div>
+</template>
+<script>
+	import mmFlag from '~/components/mm-components/flag/MmFlag.vue'
+	export default {
+		layout: 'layout-components',
+		components: {
+			mmFlag,
+		},
+		data() {
+			return {
+        log: console.log,
+				test: 0,
+				model: 0,
+				modelStrict: 1,
+			}
+		},
+	}
+</script>
+
+<style scoped>
+	.gutter {
+		column-gap: 20px;
+		row-gap: 20px;
+	}
+</style>
