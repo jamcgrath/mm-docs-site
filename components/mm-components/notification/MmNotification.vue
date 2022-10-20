a789ade5e6787635f84740bb462f535ac6929013
Trinmar Boado <timi.boado@gmail.com>
2022-10-20 08:08:56 +0800

    draft fix

--- components/mm-components/notification/MmNotification.vue
+++ components/mm-components/notification/MmNotification.vue
@@ -1,28 +1,33 @@
 <template>
 	<div role="link" tabindex="0" class="notification flex p-4 cursor-pointer" :class="{ 'grape-10-bg': !read, 'gray-1-bg': read }" @click="$emit('click')">
-		<div class="logo br-12 flex mr-4">
-      <img v-if="image" class="logo br-12" :src="image">
+		<div class="logo br-12 flex mr-4" :class="{ 'br-100': circle }">
+      <img v-if="image" class="logo br-12" :class="{ 'br-100': circle }" :src="image">
       <i v-else aria-hidden="true" :class="icon"></i>
     </div>
     <div class="flex-1 flex-col flex">
-      <div class="title-message">
-        <span class="label_med navy-dark medium title-message">{{ label }}</span>
-        <span class="label gray-6 regular title-message">{{ subLabel }}</span>
+      <div class="title-message label gray-6 regular">
+        {{ preLabel }}
+        <span class="label_med navy-dark medium title-message">{{ label }}</span>{{ removeSpaceColon }}{{ subLabel }}
       </div>
-      <div class="flex gray-5 mr-5 a-items-center overline-small time">
-        {{ time }}
+      <div class="flex gray-5 mr-1 a-items-center overline-small time">
+        <div class="flex-1">{{ time }}</div>
+        <button class="btn btn-sm btn-circle a-self-end menu"
+          :style="{ 'background-color': read ? 'var(--gray-1)' : 'var(--grape-0)' }"
+          aria-label="menu" @click.stop="$emit('menuClick')"
+        >
+          <i aria-hidden="true" class="mmi mmi-menu-horizontal-lite gray-5"></i>
+          <!-- <v-slot name="menuContent"></v-slot> -->
+        </button>
       </div>
     </div>
-    <button type="button" class="btn btn-sm btn-circle menu a-self-end" aria-label="menu" @click.stop="$emit('menuClick')">
-      <i aria-hidden="true" class="mmi mmi-menu-horizontal-lite gray-5"></i>
-      <v-slot name="menuContent"></v-slot>
-    </button>
+    
 	</div>
 </template>
 
 <script>
 	export default {
 		props: {
+      circle: Boolean,
 			icon: {
         type: String,
         default: 'mmi mmi-podcast m-auto'
@@ -30,11 +35,18 @@
       image: {
         type: String,
       },
+      preLabel: {
+        type: String,
+      },
       label: {
         type: String,
         default: 'Mamamia Out Loud'
       },
       subLabel: {
+        type: String,
+        default: 'new episode: Episode Title'
+      },
+      subLabel2: {
         type: String,
         default: 'new episode: Episode Title'
       },
@@ -54,6 +66,9 @@
 		mounted() {
 		},
 		computed: {
+      removeSpaceColon () {
+        return this.subLabel.charAt(0) === ':' ? '' : ' '
+      }
 		},
 		methods: {
 		},
@@ -62,7 +77,8 @@
 
 <style scoped>
 .menu {
-  background-color: none;
+  /* background-color: none; */
+  position: relative;
 }
 .msg {
   flex-grow: 1;
--- pages/components/notification.vue
+++ pages/components/notification.vue
@@ -1,9 +1,22 @@
 <template>
 	<div class="gutter">
-		<MmNotification label="Trinmar Pogi" sub-label="ang pogi" @click="log('clicked notif')" @menuClick="log('menu')"/>
+		<MmNotification
+			label="Mamamia Out Loud"
+			sub-label="new episode: The episode title goes here"
+			@click="log('clicked notif')" @menuClick="log('menu')"/>
 		<MmNotification read @click="log('clicked notif')" @menuClick="log('menu')" />
 		<MmNotification v-model="model" disabled @click="log('clicked notif')" @menuClick="log('menu')" />
-		<MmNotification label="Trinmar Pogi" sub-label="ang pogi" image="https://picsum.photos/200/300" @click="log('clicked notif')" @menuClick="log('menu')"/>
+		<MmNotification
+			preLabel="New subscriber exclusive episode of"
+			label="Mamamia Out Loud"
+			sub-label=": The episode title goes here"
+			image="https://picsum.photos/200/300"
+			@click="log('clicked notif')" @menuClick="log('menu')"/>
+		<MmNotification
+			image="https://picsum.photos/201/300"
+			preLabel="New video featuring" label="Author Name" subLabel=": Video title"
+			circle
+		/>
 	</div>
 </template>
 <script>
