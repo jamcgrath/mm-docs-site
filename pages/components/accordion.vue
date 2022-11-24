fda802eb13cc431e517281f8afa37774122d2225
James <jamcgrath@users.noreply.github.com>
2022-11-24 14:55:20 +0900

    fix accordion to work with screen reader

--- pages/components/accordion.vue
+++ pages/components/accordion.vue
@@ -1,25 +1,32 @@
 <template>
-  <div class="gutter">
-    <mmAccordion initial-open="true">
-      Trinmar Pogi talaga!
-    </mmAccordion>
-    <mmAccordion class="pt-5"></mmAccordion>
-    <mmAccordion class="pt-5"></mmAccordion>
-  </div>
+	<div class="gutter">
+		<mmAccordion class="pt-5">
+			<template v-slot:title> title-test </template>
+			<template v-slot:content>bork</template>
+		</mmAccordion>
+		<mmAccordion class="pt-5" initial-open="true"
+			><template v-slot:title> title-test 1 </template>
+			<template v-slot:content>bork 2</template></mmAccordion
+		>
+		<mmAccordion class="pt-5">
+			<template v-slot:title> title-test 2</template>
+			<template v-slot:content>bork 2</template>
+		</mmAccordion>
+	</div>
 </template>
 <script>
-  import MmAccordion from '~/components/mm-components/accordion/MmAccordion.vue'
+	import MmAccordion from '~/components/mm-components/accordion/MmAccordion.vue'
 	export default {
 		layout: 'layout-components',
-    components: {
-      MmAccordion
-    }
+		components: {
+			MmAccordion,
+		},
 	}
 </script>
 
 <style scoped>
-.gutter {
-  column-gap: 20px;
-  row-gap: 20px;
-}
+	.gutter {
+		column-gap: 20px;
+		row-gap: 20px;
+	}
 </style>
